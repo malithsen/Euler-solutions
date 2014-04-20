4ca2a636761ffc79c9a0cf744050bde269e310c6
Malith Senaweera <malithsenaweera@gmail.com>
2014-04-20 11:43:36 +0530

    Farey sequence

--- level72.py
+++ level72.py
@@ -1,9 +1,35 @@
-from fractions import gcd
-from tqdm import *
+### ATTEMPT 1 ###
 
-counter = 0
-for i in tqdm(range(1, 1000001)):
-    for j in range(1, i):
-        if gcd(i, j) == 1:
-            counter += 1
-print counter
+# from fractions import gcd
+# from tqdm import *
+
+# counter = 0
+# for i in tqdm(range(1, 1000001)):
+#     for j in range(1, i):
+#         if gcd(i, j) == 1:
+#             counter += 1
+# print counter
+
+### ATTEMPT 2 ###
+# def farey(n):
+#     a, b, c, d = 0, 1, 1, n
+#     counter = -1
+#     while c <= n:
+#         k = int((n + b)/d)
+#         a, b, c, d = c, d, k*c - a, k*d - b
+#         counter += 1
+#     print counter
+
+# farey(1000000)
+
+### ATTEMPT 3 ###
+limit = 1000000
+phi = [x for x in xrange(limit+1)]
+result = 0
+for x in xrange(2, limit+1):
+    if phi[x] == x:
+        for y in xrange(x, limit+1, x):
+            phi[y] = phi[y] / x * (x - 1)
+    result += phi[x]
+
+print result

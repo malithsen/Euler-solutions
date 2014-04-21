ffd4512376a4efb80fb53b320eff171b8b17e492
Malith Senaweera <malithsenaweera@gmail.com>
2014-04-21 19:04:33 +0530

    Euler's totient function

--- level70.py
+++ level70.py
@@ -1,24 +1,26 @@
-import math
-from fractions import gcd
+### ATTEMPT 1 ###
 
-limit = 100
+# import math
+# from fractions import gcd
 
-
-def isprime(n):
-    if not n % 2 and n > 2:
-        return False
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        if not n % i:
-            return False
-    return True
+# limit = 100
 
 
-def relp(n):
-    counter = 0
-    for i in xrange(1, n):
-        if gcd(n, i) == 1:
-            counter += 1
-    return counter
+# def isprime(n):
+#     if not n % 2 and n > 2:
+#         return False
+#     for i in range(3, int(math.sqrt(n)) + 1, 2):
+#         if not n % i:
+#             return False
+#     return True
+
+
+# def relp(n):
+#     counter = 0
+#     for i in xrange(1, n):
+#         if gcd(n, i) == 1:
+#             counter += 1
+#     return counter
 
 
 def isperm(n, m):
@@ -30,30 +32,54 @@ def isperm(n, m):
         return True
     return False
 
-primes = [x for x in xrange(3, 10000000 + 1, 2) if isprime(x)]
-# print primes
-print "primes table formulated"
-tortient = []
-prod = 2
-for i in primes:
-    # print prod
-    prod *= i
-    if prod < limit:
-        tortient.append(prod)
-    else:
-        break
-# print tortient
-ans = 10000000
-blah = []
-for x in tortient:
-    y = relp(x)
-    if isperm(x, y):
-        div = float(x) / float(y)
-        print div
-        if div < ans:
-            ans = div
-            print x, div
+# primes = [x for x in xrange(3, 10000000 + 1, 2) if isprime(x)]
+# # print primes
+# print "primes table formulated"
+# totient = []
+# prod = 2
+# for i in primes:
+#     # print prod
+#     prod *= i
+#     if prod < limit:
+#         totient.append(prod)
+#     else:
+#         break
+# # print totient
+# ans = 10000000
+# blah = []
+# for x in totient:
+#     y = relp(x)
+#     if isperm(x, y):
+#         div = float(x) / float(y)
+#         print div
+#         if div < ans:
+#             ans = div
+#             print x, div
 
-# print blah
-# print relp(87109)
-# print isperm(123, 2134)
+# # print blah
+# # print relp(87109)
+# # print isperm(123, 2134)
+
+### ATTEMPT 2 ###
+
+limit = 10000000
+phi = [x for x in xrange(limit+1)]
+ratio = limit
+answer = 0
+for x in xrange(2, limit+1):
+    if phi[x] == x:
+        for y in xrange(x, limit+1, x):
+            phi[y] = phi[y] / x * (x - 1)
+
+print "totient list formulated"
+phi = phi[1:]
+for i in xrange(2, limit+1):
+    k = phi[i-1]
+    if isperm(i, k):
+        frac = float(i)/float(k)
+        if frac < ratio:
+            ratio = frac
+            answer = i
+            # print i, k, frac
+
+print answer
